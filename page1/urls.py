1c99cba856b38ce93e329f57c3a2086e26deffb8
pan-dya <pandya.limawan@gmail.com>
2024-02-16 10:33:40 +0700

    create update and delete function

--- page1/urls.py
+++ page1/urls.py
@@ -4,6 +4,12 @@ from .views import *
 urlpatterns = [
     path('', placeholder,name='index'),
     path('my-form/', my_view, name='my-form'),
-    path('display-cus/',display_customer, name='Customer Data'),
-    path('display-sup/',display_supplier, name='Supplier Data'),
+    path('display_customer/',display_customer, name='Customer Data'),
+    path('display_supplier/',display_supplier, name='Supplier Data'),
+    path('customer_detail/<int:cust_id>/', customer_detail, name='customer_detail'),
+    path('edit_customer/<int:cust_id>/', edit_customer, name='edit_customer'),
+    path('delete_customer/<int:cust_id>/', delete_customer, name='delete_customer'),
+    path('supplier_detail/<int:cust_id>/', supplier_detail, name='supplier_detail'),
+    path('edit_supplier/<int:cust_id>/', edit_supplier, name='edit_supplier'),
+    path('delete_supplier/<int:cust_id>/', delete_supplier, name='delete_supplier'),
 ]
--- page1/views.py
+++ page1/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CustomerForm,PIC_Forms
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse  
 from .models import *
 # Create your views here.
 def placeholder(request):
@@ -25,3 +25,63 @@ def display_customer(request):
 def display_supplier(request):
     suppliers = Supplier.objects.all()
     return render(request, 'display_supplier.html', {'suppliers': suppliers})
+
+def customer_detail(request, cust_id):
+    customer = get_object_or_404(Customer, cust_id=cust_id)
+    form = CustomerForm(instance=customer)
+    return render(request, 'customer_detail.html', {'customer': customer, 'form': form})
+
+def edit_customer(request, cust_id):
+    customer = get_object_or_404(Customer, cust_id=cust_id)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    else:
+        form = CustomerForm(instance=customer)
+
+    return render(request, 'yourapp/edit_customer.html', {'form': form})
+
+# yourapp/views.py
+def delete_customer(request, cust_id):
+    customer = get_object_or_404(Customer, cust_id=cust_id)
+
+    if request.method == 'POST':
+        customer.delete()
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False, 'message': 'Invalid request method'})
+    
+def supplier_detail(request, cust_id):
+    customer = get_object_or_404(Supplier, cust_id=cust_id)
+    form = SupplierForm(instance=customer)
+    return render(request, 'customer_detail.html', {'customer': customer, 'form': form})
+
+def edit_supplier(request, cust_id):
+    customer = get_object_or_404(Supplier, cust_id=cust_id)
+
+    if request.method == 'POST':
+        form = SupplierForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    else:
+        form = SupplierForm(instance=customer)
+
+    return render(request, 'yourapp/edit_customer.html', {'form': form})
+
+# yourapp/views.py
+def delete_supplier(request, cust_id):
+    customer = get_object_or_404(Supplier, cust_id=cust_id)
+
+    if request.method == 'POST':
+        customer.delete()
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False, 'message': 'Invalid request method'})
