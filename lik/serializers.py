31d7663aae5a0de2e0b89ff51bbd945b1f582945
pan-dya <pandya.limawan@gmail.com>
2024-04-26 16:57:27 +0700

    fix

--- lik/serializers.py
+++ lik/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Report, Lokasi, Tujuan
-from drf_extra_fields.fields import Base64ImageField
 
 class ReportSerializer(serializers.ModelSerializer):
     foto = serializers.ImageField()
