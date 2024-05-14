a42b630b8b3fd3cec84c4238c0caa8805a8b325e
pan-dya <pandya.limawan@gmail.com>
2024-05-14 09:34:34 +0700

    LS-93 Update Styling of Prospect Page

--- page1/views.py
+++ page1/views.py
@@ -1491,7 +1491,9 @@ def prospect_ticket(request, prospect_id):
     prospect = get_object_or_404(Prospect, prospect_id=prospect_id)
     prospect_tickets = ProspectTicket.objects.filter(prospect_id=prospect).order_by('-date')
 
-    for prospect_ticket in prospect_tickets:
+    sorted_tickets = sorted(prospect_tickets, key=lambda x:(not x.open))
+
+    for prospect_ticket in sorted_tickets:
         prospect_ticket.sorted_logs = prospect_ticket.ticketlog_set.order_by('-date')
     
     ticket_log_form = TicketLogForm()
@@ -1505,7 +1507,7 @@ def prospect_ticket(request, prospect_id):
             ticket_log_form.save()
             return redirect(request.path)
 
-    context = {'prospect': prospect, 'prospect_id': prospect_id, 'prospect_tickets': prospect_tickets, 'ticket_log_form': ticket_log_form,}
+    context = {'prospect': prospect, 'prospect_id': prospect_id, 'prospect_tickets': sorted_tickets, 'ticket_log_form': ticket_log_form,}
 
     return render(request, 'prospect/prospect_ticket.html', context)
 
