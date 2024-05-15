729aabe0e2c12ac3dca4c9be737295fb57ab7724
Theotrgl <91537576+Theotrgl@users.noreply.github.com>
2024-05-15 17:29:29 +0700

    Delete page1 directory

--- page1/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class Page1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'page1'
-
-    def ready(self):
-        from . import signals
--- page1/cron.py
+++
@@ -1,4 +0,0 @@
-from django.core.management import call_command
-
-class MyCronJob():
-    call_command("reminder")
