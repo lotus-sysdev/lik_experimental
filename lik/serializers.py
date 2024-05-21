bfe3f897d2368a61e5d05550d3f62215995c0d22
pan-dya <pandya.limawan@gmail.com>
2024-05-21 11:08:26 +0700

    Merge issue manual fix

--- lik/serializers.py
+++ lik/serializers.py
@@ -14,7 +14,7 @@ class ReportSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username', 'first_name','email', 'password']
+        fields = ['id','username', 'first_name', 'email', 'password']
 
         extra_kwargs = {'password': {'write_only': True}}
 
