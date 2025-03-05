7240ceae99fff3f2732f58d2615165d6cd8c8e56
Justin <justintheofilus@gmail.com>
2025-03-05 14:36:25 +0000

    exception of static folder

--- LotusSolusindo/settings.py
+++ LotusSolusindo/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/home/justin/Lotus-Solusindo/media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
